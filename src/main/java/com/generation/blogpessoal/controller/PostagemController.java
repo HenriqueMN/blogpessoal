7aabc2e1bf69278ab2dc071d3dc537d21ae9f85a
Henrique M. N. <henriquemancininevado@gmail.com>
2023-12-11 15:06:59 -0300

    Implementação do CRUD

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 ﻿package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 /*
  * ----- ANOTAÇÕES DA CLASSE
@@ -36,6 +47,9 @@ public class PostagemController {
 	 * @GetMapping: mapeia todas as Requisições HTTP GET, enviadas para um endereço específico, chamado endpoint, dentro do Recurso Postagem, para um Método específico que responderá a requisição, ou seja, ele indica que o Método getAll(), responderá a todas as requisições do tipo HTTP GET, enviadas no endereço.
 	 * * O Endereço do endpoint será igual ao Endereço do Recurso (@RequestMapping) apenas quando a anotação @GetMapping não possuir um endereço personalizado, como um parâmetro por exemplo. Caso existam dois ou mais Métodos do tipo GET será necessário personalizar o endereço de cada Método anotado com @GetMapping, pois não pode existir endereços duplicados.
 	 * 
+	 * @PathVariable: informa que a variável usada será especificada no path da URL
+	 * 
+	 * @Valid: verifica se a requisição está de acordo com as regras definidas na model
 	 */
 	
 	@Autowired
@@ -44,5 +58,43 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){ // O Método getAll() será do tipo ResponseEntity porquê ele responderá a Requisição HTTP (HTTP Request), com uma Resposta HTTP (HTTP Response).
 		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+	
+	@GetMapping("/{id}") // É como se uma variável estivesse no path
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		// Verifica se a postagem foi encontrada, se sim, mostra a postagem para o usuário, caso contrário, retorna o status 404
+		return postagemRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); // Status 404: not foudn
+	}
+		
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+		
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) { // Envia status 400: bad request caso inválido
+		return ResponseEntity.status(HttpStatus.CREATED) // Status 201: created
+				.body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK) // Status 200: OK
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Status 204: no content
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);				
 	}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,33 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
+	
+	// JpaRepository: repositório que já tem todos os métodos necessários para a criação de um CRUD
+	
+	/* ----- Principais métodos da classe JpaRepository
+	 * 
+	 * 
+	 * save(Objeto objeto): cria ou Atualiza um objeto no Banco de Dados.
+	 * 
+	 * findById(Long id): retorna (exibe) um Objeto persistido de acordo com o id informado.
+	 * 
+	 * existsById(Long id): retorna True se um Objeto identificado pelo id estiver persistido no Banco de dados.
+	 * 
+	 * findAll(): retorna (exibe) todos os Objetos persistidos.
+	 * 
+	 * deleteById(Long id): localiza um Objeto persistido pelo id e deleta caso ele seja encontrado. Não é possível desfazer esta operação.
+	 * 
+	 * deleteAll(): deleta todos os Objetos persistidos.Não é possível desfazer esta operação.
+	 */
+	
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
