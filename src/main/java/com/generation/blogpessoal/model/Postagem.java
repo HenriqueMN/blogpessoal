e7519778aceff2ddf4134f683709051b301e6b61
Henrique M. N. <henriquemancininevado@gmail.com>
2023-12-20 13:16:46 -0300

    Implementação de usuário e da segurança

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -65,6 +65,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
     public Long getId() {
         return this.id;
@@ -105,5 +109,12 @@ public class Postagem {
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
-  
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}  
 }
