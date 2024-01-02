68e9d99fedda723dbac2c1da957a3f64b5bea7dc
Henrique M. N. <henriquemancininevado@gmail.com>
2024-01-02 14:07:29 -0300

    Implementação de testes para usuário

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -30,20 +30,41 @@ public class Usuario {
 	private String nome;
 
 	@NotNull(message = "O Atributo Usuário é Obrigatório!")
-	@Email(message = "O Atributo Usuário deve ser um email válido!") // Indica que o campo precisa estar no formato de email
+	@Email(message = "O Atributo Usuário deve ser um email válido!") // Indica
+																		// que o
+																		// campo
+																		// precisa
+																		// estar
+																		// no
+																		// formato
+																		// de
+																		// email
 	private String usuario;
 
 	@NotBlank(message = "O Atributo Senha é Obrigatório!")
 	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
 
-	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+	@Size(max = 5000,
+			message = "O link da foto não pode ser maior do que 5000 caracteres")
 	@Column(length = 5000)
 	private String foto;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario",
+			cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() {
+	}
 
 	public Long getId() {
 		return this.id;
